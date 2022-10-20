d9a4456d33a861356d6bba2ac88b596a8a4cd01b
Jhomar Custodio <jhomar@sitesphil.com>
2022-10-20 10:17:54 +0800

    Add count text

--- js/fetchdata.js
+++ js/fetchdata.js
@@ -1,6 +1,7 @@
 let ctr_group1 = 0;
 let ctr_group2 = 0;
 let ctr_group3 = 0;
+let countUnderThreshold = 0;
 
 $(()=>{
     //Initial Request for USD, the default selected value
@@ -13,17 +14,14 @@ $(()=>{
     }
 
     fetchdata(selected_item,currencies,exchange_rates);
-    // countChild('div#group1',ctr_group1);
-    // countChild('div#group2',ctr_group2);
-    // countChild('div#group3',ctr_group3);
 
     $('select').change((event)=>{
         selected_item = event.target.value;
         clearGroup();
+        clearCount();
+        clearThreshold();
         fetchdata(selected_item,currencies,exchange_rates);
-        // countChild('div#group1',ctr_group1);
-        // countChild('div#group2',ctr_group2);
-        // countChild('div#group3',ctr_group3);
+
     });
    
 });
@@ -83,17 +81,22 @@ const groupData = (selected_item,item,data) => {
         </tr>
         `;
 
+        countLessThanThreshold(data[item]);
+
         if( data[item] < 1){
             $('div#group1>table').append(new_element);
-            ctr_group1 += 1;
+            ctr_group1++;
+            countChild('div#group1 span',ctr_group1);
         }
         else if(data[item] >= 1 && data[item] < 1.5){
             $('div#group2>table').append(new_element);
-            return ctr_group2 += 1;
+            ctr_group2++;
+            countChild('div#group2 span',ctr_group2);
         }
         else if(data[item] >= 1.5){
             $('div#group3>table').append(new_element);
-            ctr_group3 += 1;
+            ctr_group3++;
+            countChild('div#group3 span',ctr_group3);
         }
         else{
             console.log('Something went wrong');
@@ -111,10 +114,29 @@ const clearGroup = () => {
 }
 
 const countChild = (target,count) => {
-    const targetElement= target + ">span";
-    console.log(count)
+    $(target).text(`Count: ${count}`);
+}
 
-    $(target).text("Count:",count);
+const clearCount = () => {
+    $('div#group1 span').text("Count: 0");
+    $('div#group2 span').text("Count: 0");
+    $('div#group3 span').text("Count: 0");
+}
+
+const countLessThanThreshold = (value) => {
+    let threshold = 1.5;
+
+    if(value <= threshold){
+        countUnderThreshold++;
+    }
+
+    let new_text = `There are ${countUnderThreshold} values <= ${threshold}`;
+    $('div#underThreshold>span').text(new_text);
+}
+
+const clearThreshold = () => {
+    countUnderThreshold = 0;
+    $('div#underThreshold>span').text();
 }
 
    
